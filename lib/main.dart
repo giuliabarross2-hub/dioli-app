9f9c179b65fdf73a28e2f5ad9e70f57d168af41d
giuliabarross2-hub <giuliabarross2@gmail.com>
2026-09-12 00:42:21 +0000

    Agiliza arraste de agendamentos

--- lib/main.dart
+++ lib/main.dart
@@ -1514,7 +1514,7 @@ class _AgendaPageState extends State<AgendaPage> {
           LongPressGestureRecognizer:
               GestureRecognizerFactoryWithHandlers<LongPressGestureRecognizer>(
             () => LongPressGestureRecognizer(
-              duration: const Duration(milliseconds: 180),
+              duration: const Duration(milliseconds: 80),
             ),
             (LongPressGestureRecognizer recognizer) {
               recognizer.onLongPressStart = (_) {
