c151bbb0c5a903c0e0c7858c60b3608a6b4de5ba
giuliabarross2-hub <giuliabarross2@gmail.com>
2026-09-14 20:10:46 +0000

    Corrige calendario para portugues

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:intl/intl.dart';
@@ -36,6 +37,15 @@ class DioliApp extends StatelessWidget {
     return MaterialApp(
       title: 'DIOLI – Studio de Beleza',
       debugShowCheckedModeBanner: false,
+      locale: const Locale('pt', 'BR'),
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
         useMaterial3: true,
         scaffoldBackgroundColor: AppColors.background,
